a18948843c5f9ac4f2df592114bfc88b86ffd5c3
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-04-05 17:58:48 -0600

    still wont work on heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,6 @@ const FILES_TO_CACHE = [
     "icons/icon-512x512.png",
 ];
 
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
@@ -17,60 +16,70 @@ self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("Your files were pre-cached successfully!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
+            cache.addAll(FILES_TO_CACHE).then(() => {
+                self.skipWaiting()
+            });
+        }),
     );
 
-    self.skipWaiting();
+    // self.skipWaiting();
 });
 
 // activate
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
+self.addEventListener("activate", event => {
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => {
+                // return array of cache names that are old to delete
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName),
+                );
+            })
+            .then(cachesToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete => {
+                        return caches.delete(cacheToDelete);
+                    }),
+                );
+            })
+            .then(() => self.clients.claim()),
     );
-
-    self.clients.claim();
 });
 
 // fetch
 self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                    .then(response => {
-                        // If the response was good, clone it and store it in the cache.
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                        .then(response => {
+                            // If the response was good, clone it and store it in the cache.
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
 
-                        return response;
-                    })
-                    .catch(err => {
-                        // Network request failed, try to get it from the cache.
-                        return cache.match(evt.request);
-                    });
-            }).catch(err => console.log(err))
+                            return response;
+                        })
+                        .catch(err => {
+                            // Network request failed, try to get it from the cache.
+                            return cache.match(evt.request);
+                        });
+                })
+                .catch(err => console.log(err)),
         );
 
         return;
     }
-
     evt.respondWith(
+        // console.log(FILES_TO_CACHE),
         caches.open(CACHE_NAME).then(cache => {
             return cache.match(evt.request).then(response => {
                 return response || fetch(evt.request);
             });
-        })
+        }),
     );
 });
