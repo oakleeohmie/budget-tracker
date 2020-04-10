c472901d8958809644d7550db29e3bcbb5201a1e
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-04-09 18:55:06 -0600

    created db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 const FILES_TO_CACHE = [
     "/",
-    "index.html",
-    "styles.css",
-    "index.js",
-    "manifest.webmanifest",
-    "icons/icon-192x192.png",
-    "icons/icon-512x512.png",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
